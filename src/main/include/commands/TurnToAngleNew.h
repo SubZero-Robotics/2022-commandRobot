3d5ae57309349db28f1d86e322e45442acf552ad
Alec Habig <ahabig@umn.edu>
2022-02-26 16:19:16 -0600

    Add a Ramsetes driven turn to angle

---
+++ src/main/include/commands/TurnToAngleNew.h
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc2/command/CommandBase.h> 
+#include <frc2/command/CommandHelper.h>
+
+#include "subsystems/DriveSubsystem.h"
+
+class TurnToAngleNew
+    : public frc2::CommandHelper<frc2::CommandBase, TurnToAngleNew> {
+ public:
+  /**
+   * Creates a new TurnToAngleNew.
+   *
+   * @param target The number of degrees to turn
+   * @param drive  The drive subsystem on which this command will run
+   */
+  TurnToAngleNew(units::degree_t target, DriveSubsystem* subsystem);
+
+  void Initialize() override;
+
+ private:
+  DriveSubsystem* m_drive;
+  units::degrees_t m_angle;
+  // Leaving this here: once we figure out how to attach a speed scaling to a 
+  // RamsetesCommand, we can put it back as a parameter
+  double m_speed;  
+};
