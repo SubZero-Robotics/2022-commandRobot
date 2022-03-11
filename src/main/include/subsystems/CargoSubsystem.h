aa4703e24db5cfe2316fc86bb73d38b1fa1162f8
Michael Cornell <77296076+MicCorn@users.noreply.github.com>
2022-03-10 22:23:43 -0600

    PutLED method added

--- src/main/include/subsystems/CargoSubsystem.h
+++ src/main/include/subsystems/CargoSubsystem.h
@@ -37,6 +37,11 @@ class CargoSubsystem : public frc2::SubsystemBase {
   void GrabBalls();
 
   /**
+   * @param ledMotorValue the value in percent out to be sent to the Blinkin
+   */
+  double PutLED(double ledMotorValue);
+
+  /**
    * All index motors in
    */
   void AutomaticIntake();
